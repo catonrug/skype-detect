6c8d99d51655802cc1d9abfe6a2da5c2b304cbbb
catonrug <aigars.kadikis@gmail.com>
2016-03-07 20:37:17 +0200

    next

--- check.sh
+++ check.sh
@@ -54,6 +54,12 @@ if [ ! -d "../gd" ]; then
   mkdir -p "../gd"
 fi
 
+if [ -f ~/uploader_credentials.txt ]; then
+sed "s/folder = test/folder = `echo $appname`/" ../uploader.cfg > ../gd/$appname.cfg
+else
+echo google upload will not be used cause ~/uploader_credentials.txt do not exist
+fi
+
 name=$(echo "Skype")
 changes=$(echo "https://support.skype.com/en/faq/FA34509/what-s-new-in-skype-for-windows-desktop")
 
